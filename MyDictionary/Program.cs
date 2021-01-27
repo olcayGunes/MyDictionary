3eefed626ed558fc328d6669b6062c3094fa732c
olcayGunes <65913895+olcayGunes@users.noreply.github.com>
2021-01-27 21:06:09 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+			myDictionary.Add(1, "Olcay Güneş");
+			myDictionary.Add(2, "Engin Demiroğ");
+			myDictionary.Add(3, "Kodlama.io");
+
+			myDictionary.ShowAll();
+		}
+	}
+}
